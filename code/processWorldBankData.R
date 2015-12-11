e766fb4b9bfac3e7808025afa8d725abe7f0489b
John Orr <jah.Norr@gmail.com>
2015-12-11 11:23:52 -0400

    added raw data

--- code/processWorldBankData.R
+++ code/processWorldBankData.R
@@ -5,7 +5,8 @@ filename<-"./data_raw/Population_World_Development_Indicators_Data.csv"
 
 df_annual_pop<- getWorldBankData(filename,series="^[Pp]opul.*,.*total$")
 
-df<-data.frame(df_annual_pop$Year,df_annual_pop$Value)
+df<-data.frame(Year=df_annual_pop$Year,Population=df_annual_pop$Value)
 
 write.csv(df,file = "./data/population_vi_wb.csv",row.names = F)
+
 
