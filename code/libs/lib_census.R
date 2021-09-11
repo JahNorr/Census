08cc35fafc9b2821f4ec3d0c3961943c73048071
John Orr <jah.Norr@gmail.com>
2021-09-11 11:54:49 -0600

    Updating

---
+++ code/libs/lib_census.R
@@ -0,0 +1,14 @@
+
+
+download.census.counties.estimates<-function(state) {
+  if(is.na(as.integer(state))) {
+    
+  }
+}
+
+# https://www2.census.gov/programs-surveys/popest/tables/2000-2010/intercensal/state/st-est00int-02-30.xls (MT)
+# https://www2.census.gov/programs-surveys/popest/tables/2000-2010/intercensal/state/st-est00int-02-01.xls (AL)
+# https://www2.census.gov/programs-surveys/popest/tables/2000-2010/intercensal/state/st-est00int-03-30.xls (MT) SexRaceEthn
+# 
+# https://www2.census.gov/programs-surveys/popest/datasets/2010-2018/counties/asrh/cc-est2018-alldata-30.csv  (Recent from above)
+
